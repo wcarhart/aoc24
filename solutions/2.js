184f47cb1284ed6ef7e2c824ea9a3f7ef56cf183
Will Carhart <carhartwill@gmail.com>
2024-12-01 21:51:28 -0800

    Add comments

--- solutions/2.js
+++ solutions/2.js
@@ -1,11 +1,14 @@
 const checkValidity = (items) => {
+    // first, check if either all increasing or all decreasing
     const sortedAccending = items.toSorted((a, b) => a - b)
     const sortedDescending = sortedAccending.map(e => e).reverse()
 
+    // this is SUPER greedy, but fine because the arrays are very small
     if (JSON.stringify(items) !== JSON.stringify(sortedAccending) && JSON.stringify(items) !== JSON.stringify(sortedDescending)) {
         return false
     }
 
+    // next, determine if all items are within 3 of each other
     let previous = null
     for (let item of items) {
         if (previous === null) {
@@ -45,6 +48,7 @@ const generatePossibilities = (items) => {
 }
 
 const partTwo = (lines) => {
+    // we'll use the same logic as part one, but we'll brute force by generating all possibilities of the lists
     let count = 0
     for (let line of lines) {
         const items = line.split(' ').map(i => Number(i))
